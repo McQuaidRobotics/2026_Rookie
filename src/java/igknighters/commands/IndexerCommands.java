a7edac212f62d1cfff2717b575d2ddfde423c1da
Mason Wolff <maseymoo09@gmail.com>
2026-09-16 22:50:43 -0400

    some goofy merging changes

--- src/java/igknighters/commands/IndexerCommands.java
+++ src/java/igknighters/commands/IndexerCommands.java
@@ -1,12 +1,11 @@
 package igknighters.commands;
 
-
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import igknighters.subsystems.indexer.Indexer;
 
-public class SpindexerCommands {
+public class IndexerCommands {
     public static Command SummonSpindexeroidTitano(
             Indexer indexer, AngularVelocity exitRollerSpeed, AngularVelocity spindexerSpeed) {
         return Commands.parallel(
