a994d0cecbb4ff79960f25284f5fb3c70ea1ce7b
Henry <henryj.winchester@outlook.com>
2026-09-22 16:15:19 -0400

    The fuel sim should be fully enabled provided that spindexer and flywheels get put in and implement what they need to

--- src/java/igknighters/subsystems/shooter/Shooter.java
+++ src/java/igknighters/subsystems/shooter/Shooter.java
@@ -1,5 +1,9 @@
 package igknighters.subsystems.shooter;
 
+import static edu.wpi.first.units.Units.RPM;
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -32,7 +36,18 @@ public class Shooter extends SubsystemBase {
                 hood.targetAngleCommand(state.hoodAngle),
                 turret.targetAngleCommand(state.turretAngle));
     }
+    // KYLE : ONCE THIS GOES INTO UR FLYWHEEL BRANCH YOU ARE GOING TO REPLACE THIS WITH A WORKING THING
+    public AngularVelocity getFlywheelVelocity() {
+        return RPM.of(0.0);
+        // return flywheel.getFlywheelVelocity(); 
+    }
 
+    public Angle getHoodAngle() {
+        return hood.getCurrentAngle();
+    }
+    public Angle getTurretAngle() {
+        return turret.getCurrentAngle();
+    }
     public boolean isHoodSensorHit() {
         return hood.getHoodLimit();
     }
